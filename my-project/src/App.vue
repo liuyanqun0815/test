934397ec11250f1497419224e16d40963e058b6e
刘艳群 <liuyanqun@meicai.cn>
2019-08-27 11:49:19 +0800

    第一版

---
+++ my-project/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>site:{{site}}</h1>
+    <h1>url:{{url}}</h1>
+    <h1>{{details()}}</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+  // var vm = new Vue({
+  //   el:'#app',
+  //   data:{
+  //     site:"教程",
+  //     url:"www.runoob.com",
+  //     alexa:"10000"
+  //   },
+  //   methods:{
+  //     details:function () {
+  //       return this.site+"-xxxxxxx-bbbbbb-ddddddd"
+  //     }
+  //   }
+  // })
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
